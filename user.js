c555a56e8fbe2b95008249b2307f1ec6666051c6
naman708 <83646450+naman708@users.noreply.github.com>
2023-11-08 22:39:55 +0530

    backend-login-api

--- user.js
+++ user.js
@@ -6,7 +6,7 @@ const authenticatemiddleware = require('../middleware/auth');
 
 router.post('/user/registeruser',userController.adduser);
 router.get('/user/existinguser/:id',userController.findingexistinguser);
-
+router.post('/user/login',userController.loginuser);
 
 
 
